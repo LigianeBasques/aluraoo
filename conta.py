194c763056b85fb9d8003958627d1059820f970d
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-13 13:30:17 -0400

    Conta OO

--- conta.py
+++ conta.py
@@ -4,9 +4,9 @@ class Conta:
 
    def __init__(self,numero, titular, saldo, limite):
        print("Construindo objeto...{}".format(self))
-       self.numero = 123
-       self.titular = "Nico"
-       self.saldo = 55.0
-       self.limte = 1000.0
+       self.numero = numero
+       self.titular = titular
+       self.saldo = saldo
+       self.limte = limite
 
 
