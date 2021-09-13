c92e760d6cd9bf4146e71a790825028555eaa4ea
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-13 12:57:48 -0400

    Conta OO

--- conta.py
+++ conta.py
@@ -2,4 +2,11 @@
 
 class Conta:
 
-    pass
+   def __init__(self):
+       print("Construindo objeto...{}".format(self))
+       self.numero = 123
+       self.titular = "Nico"
+       self.saldo = 55.0
+       self.limte = 1000.0
+
+
