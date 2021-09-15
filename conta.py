3cc060e81a7f0813fb84a1a3fb29d7f3626afeec
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-15 07:53:25 -0400

    Conta

--- conta.py
+++ conta.py
@@ -4,17 +4,17 @@ class Conta:
 
    def __init__(self,numero, titular, saldo, limite):
        print("Construindo objeto...{}".format(self))
-       self.numero = numero
-       self.titular = titular
-       self.saldo = saldo
-       self.limte = limite
+       self.__numero = numero
+       self.__titular = titular
+       self.__saldo = saldo
+       self.__limte = limite
    def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo,self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo,self.__titular))
 
    def deposita(self,valor):
-       self.saldo += valor
+       self.__saldo += valor
    def saca(self,valor):
-       self.saldo -= valor
+       self.__saldo -= valor
 
 
 
