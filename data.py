862f8994a993783ea1e1e2b3acebe97d1e92c602
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-15 07:41:36 -0400

    data

---
+++ data.py
@@ -0,0 +1,14 @@
+
+class Data:
+
+    def __init__(self,dia, mes, ano):
+        self.dia = 21
+        self.mes = 11
+        self.ano = 2007
+    def data(self):
+        print(" {}/ {}/ {}".format(self.dia, self.mes, self.ano))
+
+
+
+
+
