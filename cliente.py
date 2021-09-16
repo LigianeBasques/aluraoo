d0c6fd39c8b46ac4dd4c949f4af591a567450082
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-16 12:22:57 -0400

    Cliente

--- cliente.py
+++ cliente.py
@@ -6,7 +6,9 @@ class Cliente:
     def nome(self):
         print("chamando @property nome()")
         return self.__nome.title()
+    @nome.setter
     def nome(self,nome):
+        print("chamando setter nome()")
         self.__nome = nome
 
 
