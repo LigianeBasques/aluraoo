be20a04d2a8e21111d68c526552d6f334606bbcf
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-16 10:46:08 -0400

    Cliente

--- cliente.py
+++ cliente.py
@@ -2,8 +2,8 @@
 class Cliente:
     def __init__(self,nome):
         self.nome = nome
-    def get_nome(self):
-        return self.nome.title()
+    def nome(self):
+       return self.nome.title()
 
 
 
