db01de4f6cdfef12a3820c011faff949ddb9579c
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-16 12:28:21 -0400

    Conta

--- conta.py
+++ conta.py
@@ -27,10 +27,12 @@ class Conta:
    def get_titular(self):
        return self.__titular
 
-   def get_limite(self):
-       return self.__limte
+   @property
+   def limite(self):
+       return self.__limite
 
-   def set_limite(self,limite):
+   @limite.setter
+   def limite(self,limite):
        self.__limte = limite
 
 
